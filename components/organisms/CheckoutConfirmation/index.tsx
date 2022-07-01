1ae2520e42eaa1ca9e63572899f8b70a424e1428
Dewan-toro <dewantoro477@gmail.com>
2022-07-02 06:38:02 +0700

    Refactor Member SideBar

--- components/organisms/CheckoutConfirmation/index.tsx
+++ components/organisms/CheckoutConfirmation/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 export default function CheckoutConfirmation() {
   return (
     <>
---
+++ components/organisms/SideBar/index.tsx
@@ -0,0 +1,23 @@
+import Footer from './Footer';
+import MenuItem from './MenuItem';
+import Profile from './Profile';
+
+export default function SideBar() {
+  return (
+    <section className="sidebar">
+      <div className="content pt-50 pb-30 ps-30">
+        <Profile />
+        <div className="menus">
+          <MenuItem title="Overview" icon="ic-menu-overview" active />
+          <MenuItem title="Transactions" icon="ic-menu-transactions" />
+          <MenuItem title="Messages" icon="ic-menu-messages" />
+          <MenuItem title="Card" icon="ic-menu-card" />
+          <MenuItem title="Reward" icon="ic-menu-reward" />
+          <MenuItem title="Settings" icon="ic-menu-settings" />
+          <MenuItem title="Log Out" icon="ic-menu-logout" />
+        </div>
+        <Footer />
+      </div>
+    </section>
+  );
+}
